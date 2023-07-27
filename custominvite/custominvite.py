8a6c8f1ac2c1e333a8e11e7a0e82fcaab6316ff2
Matt H <36822930+MattHardwick34@users.noreply.github.com>
2023-07-27 19:35:44 +0100

    Update custominvite.py

--- custominvite/custominvite.py
+++ custominvite/custominvite.py
@@ -5,6 +5,12 @@ class CustomInvite(commands.Cog):
 
     def __init__(self, bot):
         self.bot = bot
+        self._invite_command: Optional[commands.Command] = self.bot.remove_command("invite")
+
+    async def cog_unload(self) -> None:
+        self.bot.remove_command("invite"), self.bot.add_command(  # type:ignore
+            self._invite_command
+        ) if self._invite_command else None
 
     @commands.command()
     async def invite(self, ctx):
