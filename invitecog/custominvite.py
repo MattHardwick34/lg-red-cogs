258fc63ccea875ba26dc1a2b686abd79bce54eec
Matt H <36822930+MattHardwick34@users.noreply.github.com>
2023-07-27 19:10:27 +0100

    Update and rename invite.py to custominvite.py

--- invitecog/custominvite.py
+++ invitecog/custominvite.py
@@ -1,13 +1,13 @@
 from redbot.core import commands
 
-class MyCog(commands.Cog):
+class CustomInvite(commands.Cog):
     """Custom invite text"""
 
     def __init__(self, bot):
         self.bot = bot
 
     @commands.command()
-    async def mycom(self, ctx):
+    async def invite(self, ctx):
         """Sends message with an invite to the server"""
         # Your code will go here
         await ctx.send("Invite your friends here? https://discord.gg/LondonGaymers")
